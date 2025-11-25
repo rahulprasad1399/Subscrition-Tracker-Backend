241f1819f1f6a48162343bc5e2bad7bc2285364b
rahul.prasad <rahul.prasad@geekywolf.com>
2025-11-25 16:45:28 +0530

    added length constraint for entity

--- Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Domain/Entity/Subscription.cs
+++ Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Domain/Entity/Subscription.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Trackify.SubscriptionTracker.Domain.Entity
 {
-    internal class Subscriptions
+    public class Subscription
     {
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public int ServiceId { get; private set; }
+        public int SubscriptionTypeId { get; private set; }
+        public decimal Cost { get; private set; }
+        public int BillingFrequency { get; private set; }
+        [Required, MaxLength(25)]
+        public string BillingPeriodUnit { get; private set; }
+        public DateTime PurchaseDate { get; private set; }
+        public DateTime RenewalDate { get; private set; }
+        public ActiveStatus Status { get; private set; }
+
+        public Subscription(int userId, int serviceId, int subscriptionTypeId, decimal cost, int billingFrequency, string billingPeriodUnit, DateTime purchaseDate, DateTime renewalDate, ActiveStatus status)
+        {
+            UserId = userId;
+            ServiceId = serviceId;
+            SubscriptionTypeId = subscriptionTypeId;
+            Cost = cost;
+            BillingFrequency = billingFrequency;
+            BillingPeriodUnit = billingPeriodUnit;
+            PurchaseDate = purchaseDate;
+            RenewalDate = renewalDate;
+            Status = status;
+        }
+
+        public enum ActiveStatus
+        {
+            Active,
+            Cancelled,
+            Paused
+        }
+
+        private Subscription() { }
     }
 }
--- Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Domain/Entity/User.cs
+++ Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Domain/Entity/User.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Trackify.SubscriptionTracker.Domain.Entity
 {
-    internal class Users
+    public class User
     {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string FullName { get; set; }
+        [EmailAddress, Required, MaxLength(100)]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        [Required, MaxLength(25)]
+        public string Role { get; set; } = "Subscriber";
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public User(string fullName, string email, string passwordHash, string role, string refreshToken, DateTime refreshTokenExpiryTime)
+        {
+            FullName = fullName;
+            Email = email;
+            PasswordHash = passwordHash;
+            Role = role;
+            RefreshToken = refreshToken;
+            RefreshTokenExpiryTime = refreshTokenExpiryTime;
+        }
+
+        private User() { }
     }
 }
