71d754680b72441eeee64d7c285ef136c246ff33
Nabeel P P <nabeel.p@geekywolf.local>
2025-11-26 11:31:28 +0530

    added category command

---
+++ Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Application/Category/Command/CreateCategoryCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trackify.SubscriptionTracker.Application.Interface;
+using Trackify.SubscriptionTracker.Domain.Entity;
+
+namespace Trackify.SubscriptionTracker.Application.Category.Command
+{
+    public class CreateCategoryCommand : IRequest<int>
+    {
+        public string CategoryName {  get; set; }
+    }
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, int>
+    {
+        private readonly IGenericRepository<Category> _repository;
+
+        public CreateCategoryCommandHandler(IGenericRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<int> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = new Category(request.CategoryName);
+            return await _repository.AddAsync();
+        }
+    }
+}
