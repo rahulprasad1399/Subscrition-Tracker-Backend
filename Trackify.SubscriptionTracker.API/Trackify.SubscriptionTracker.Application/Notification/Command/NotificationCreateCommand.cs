ec8e7dfea69a24b687ea19cdc90eb1a758a7d439
rahul.prasad <rahul.prasad@geekywolf.com>
2025-12-05 18:24:37 +0530

    completed real time notification

--- Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Application/Notification/Command/NotificationCreateCommand.cs
+++ Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Application/Notification/Command/NotificationCreateCommand.cs
@@ -1,11 +1,5 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Trackify.SubscriptionTracker.Application.Interface;
 using Trackify.SubscriptionTracker.Application.NotificationDto;
 using Trackify.SubscriptionTracker.Domain.Entity;
--- Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Infrastructure/Services/NotficationHubService.cs
+++ Trackify.SubscriptionTracker.API/Trackify.SubscriptionTracker.Infrastructure/Services/NotficationHubService.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Trackify.SubscriptionTracker.APIHub;
 using Trackify.SubscriptionTracker.Application.Interface;
 using Trackify.SubscriptionTracker.Application.NotificationDto;
